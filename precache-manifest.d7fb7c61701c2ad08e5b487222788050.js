8c094d874aebf554d5768beb2b508dd9602fa37c
abc <you@example.com>
2019-10-26 23:04:14 +0100

    Updates

---
+++ precache-manifest.d7fb7c61701c2ad08e5b487222788050.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cbd1c5a2a85268b1723b3266135a1c8",
+    "url": "/cryptoInfo/index.html"
+  },
+  {
+    "revision": "e6a8746a5f1572fc838b",
+    "url": "/cryptoInfo/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "adaa894c4f932acc6aee",
+    "url": "/cryptoInfo/static/css/main.31670647.chunk.css"
+  },
+  {
+    "revision": "e6a8746a5f1572fc838b",
+    "url": "/cryptoInfo/static/js/2.a8c22d67.chunk.js"
+  },
+  {
+    "revision": "adaa894c4f932acc6aee",
+    "url": "/cryptoInfo/static/js/main.f55a6e8b.chunk.js"
+  },
+  {
+    "revision": "b70b9af160ea8698becf",
+    "url": "/cryptoInfo/static/js/runtime-main.994e6146.js"
+  }
+]);
